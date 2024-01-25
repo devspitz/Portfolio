42119911160c0b22631e2f68b5351d7b0745c9a5
devspitz <spitzdev@gmail.com>
2024-01-24 21:29:37 -0500

    contact

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -69,7 +69,7 @@ export default function Contact() {
                         <h2>Let's talk :)</h2>
                         <p>Devorah Kogan</p>
 
-                        <a href="tel:2168674782" className='btn'><i className="fa-solid fa-phone"></i> Call 216-867-4782</a>
+                        <a href="tel:2168674782" className='btn'><i className="fa-solid fa-phone"></i> 216-867-4782</a>
 
                         <a href="mailto:spitzdev@gmail.com" className='btn'><i className="fa-solid fa-envelope"></i> Email</a>
 
@@ -82,16 +82,16 @@ export default function Contact() {
                         <form className='contactForm' onSubmit={handleSubmit}>
 
                             <label htmlFor="name">Name</label>
-                            <input type='text' value={formName} name="name" onChange={e => setFormName(e.target.value)} />
+                            <input type='text' required value={formName} name="name" onChange={e => setFormName(e.target.value)} />
 
                             <label htmlFor="email">Email</label>
-                            <input type='text' value={formEmail} name="email" onChange={e => setFormEmail(e.target.value)} />
+                            <input type='text' required value={formEmail} name="email" onChange={e => setFormEmail(e.target.value)} />
 
                             <label htmlFor="phone">Phone</label>
                             <input type='text' value={formPhone} name="phone" onChange={e => setFormPhone(e.target.value)} />
 
                             <label htmlFor="msg">Message</label>
-                            <input type='textfield' value={formMsg} name="msg" onChange={e => setFormMsg(e.target.value)} />
+                            <input type='textfield' required value={formMsg} name="msg" onChange={e => setFormMsg(e.target.value)} />
                             <button className='btn'>Send <i className="fa-solid fa-arrow-right"></i></button>
                         </form>
                     }
